4d0306b93714f99e3cec619c1127664db51a9052
Agata <bagata134@gmail.com>
2021-06-06 12:33:28 +0200

    Pierwsza wersja, nawigacja

---
+++ app/src/main/java/com/example/dobrypiesek/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.dobrypiesek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.home2, R.id.profile, R.id.calendar))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
